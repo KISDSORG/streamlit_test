9cff8b4617c1700f1d258936461d17c4ca755cff
chammmiii <58718071+chammmiii@users.noreply.github.com>
2022-12-05 17:02:35 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+import streamlit as st
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+    st.title('st test success!')
+    st.write("hello world")
+    st.write("""
+    # MarkDown
+    > comment
+    - one
+    - two
+    - three
+    """)
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
