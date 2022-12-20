63a8b05dcdfac9df8a564fd027b34fb336f04ba3
chammmiii <58718071+chammmiii@users.noreply.github.com>
2022-12-20 10:04:57 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import pandas as pd
 from bs4 import BeautifulSoup
 
 api_key = 'd7d1be298b9cac1558eab570011f2bb40e2a6825'
+st.set_option('wideMode', True)
 
 def get_corp_code(corp, start, end):
     # 기업 고유번호 가져오기
@@ -107,7 +108,6 @@ if __name__ == '__main__':
     if st.button('조회'):
         df = get_corp_code(corp, start, end)
         st.dataframe(df)
-        st.set_option('wideMode', True)
 
         csv = convert_df(df)
 
