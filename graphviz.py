9a290eb003997fbd1b79b774b0c2b93b4364ae4f
chammmiii <58718071+chammmiii@users.noreply.github.com>
2023-02-24 10:34:03 +0900

    Update graphviz.py

--- graphviz.py
+++ graphviz.py
@@ -10,7 +10,7 @@ if uploaded_file is not None:
     st.write(bytes_data)
 
     # To convert to a string based IO:
-    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+    stringio = StringIO(uploaded_file.getvalue().decode("utf-8-sig"))
     st.write(stringio)
 
     # To read file as string:
