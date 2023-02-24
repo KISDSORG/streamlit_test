b461b2b06a095c51b8a230becb0b2f18080147f8
chammmiii <58718071+chammmiii@users.noreply.github.com>
2023-02-24 16:41:33 +0900

    Update graphviz.py

--- graphviz.py
+++ graphviz.py
@@ -1,22 +1,11 @@
 import streamlit as st
 import pandas as pd
-from io import StringIO
 
-uploaded_file = st.file_uploader("Choose a file")
+uploaded_file = st.file_uploader("Choose a file", type='xlsx')
 
 if uploaded_file is not None:
-    # To read file as bytes:
-    bytes_data = uploaded_file.getvalue()
-#     st.write(bytes_data)
 
-    # To convert to a string based IO:
-    stringio = StringIO(uploaded_file.getvalue().decode("cp949"))
-    st.write(stringio)
+    df = pd.read_excel(uploaded_file)
 
-    # To read file as string:
-    string_data = stringio.read()
-    st.write(string_data)
-
-    # Can be used wherever a "file-like" object is accepted:
-    dataframe = pd.read_csv(uploaded_file)
-    st.write(dataframe)
+    st.dataframe(df)
+    st.table(df)
